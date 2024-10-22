07b40bff3b28356c88f105cf065c836ce6626645
Cleverland197710 <realzacharyjayden@gmail.com>
2024-10-22 09:55:39 +0100

    3rd push

--- 3D Platformer/Assets/Scripts/PlayerMovement3D.cs
+++ 3D Platformer/Assets/Scripts/PlayerMovement3D.cs
@@ -19,24 +19,24 @@ public class PlayerMovement3D : MonoBehaviour
         }
 
         
-        if (Input.GetKey("w"))
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 3);
         }
 
-        if (Input.GetKey("s"))
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5);
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -3);
         }
 
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(-5, 0, 0);
+            GetComponent<Rigidbody>().velocity = new Vector3(-3, 0, 0);
         }
 
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0);
+            GetComponent<Rigidbody>().velocity = new Vector3(3, 0, 0);
         }
 
     }
