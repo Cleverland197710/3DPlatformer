bfff4dc3e6cb88091232d9096a148e59ab5c1f14
Cleverland197710 <realzacharyjayden@gmail.com>
2024-11-18 15:24:42 +0000

    12th

--- 3D Platformer/Assets/Scripts/Movement.cs
+++ 3D Platformer/Assets/Scripts/Movement.cs
@@ -7,26 +7,53 @@ public class Movement : MonoBehaviour
     Rigidbody rb;
     public float moveSpeed = 10f;
     public float jumpForce = 10f;
+
+    [SerializeField] float mouseSensitivity = 100f;
+    float xRotation = 0f;
+    [SerializeField] Transform playerCamera; // Assign your camera in the Inspector
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
     public
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();   
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked; // Lock cursor in place
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Handle mouse look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Clamp vertiacl rotation
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Rotate camera up/down
+        transform.Rotate(Vector3.up * mouseX); // Rotate player left/right
+
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(horizontalInput * moveSpeed, rb.velocity.y, verticalInput * moveSpeed);
+        // Move in the direction the player is facing
+        Vector3 moveDirection = transform.right * horizontalInput + transform.forward * verticalInput;
+        rb.velocity = new Vector3(moveDirection.x * moveSpeed, rb.velocity.y, moveDirection.z * moveSpeed);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
         }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, .1f, groundLayer);
     }
 }
