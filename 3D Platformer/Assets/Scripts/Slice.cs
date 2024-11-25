02bbf5ce12f89c7d6efd9e053d26258f4e3aba0f
Cleverland197710 <realzacharyjayden@gmail.com>
2024-11-25 10:45:08 +0000

    19th

--- 3D Platformer/Assets/Scripts/Slice.cs
+++ 3D Platformer/Assets/Scripts/Slice.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class Slice : MonoBehaviour
 {
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.compareTag("Kill"))
+        if (collision.gameObject.CompareTag("Kill"))
         {
             Destroy(collision.transform.parent.gameObject);
         }
